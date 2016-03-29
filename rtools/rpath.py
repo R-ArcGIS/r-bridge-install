c721bc0a20b3e5460c6062ae982083cfdc8eb5eb
Shaun Walbridge <shaun.walbridge@gmail.com>
2016-03-29 01:14:46 -0400

    Rewrite detection logic for ArcGIS Desktop
    Updated two functions to improve handling of ArcGIS installation
    details:
    
    `arcmap_exists` is updated to check HKLM, which contains the install
    information, not HKCU, which only contains user settings and will
    persist even after the product is removed. Also now checks both the
    32- and 64-bit hives on 64-bit platforms.
    
    `arcmap_path` now defaults to searching all known ArcGIS versions that
    can support the bridge, and will return the first one it finds. Because
    of how ArcGIS is installed, only one instance can exist on the machine.

--- rtools/rpath.py
+++ rtools/rpath.py
@@ -387,50 +387,22 @@ r_package_version = r_pkg_version()
 
 
 def arcmap_exists(version=None):
-    root_key = winreg.HKEY_CURRENT_USER
-    reg_base = "SOFTWARE\\Esri"
-    if not version:
-        version = "10.3"
-    package_key = "Desktop{}".format(version)
+    """Check for the existence of the specified version of ArcMap.
 
-    reg_path = "{}\\{}".format(reg_base, package_key)
-    arcmap_reg = None
-    installed = False
-    try:
-        # find the key, 64- or 32-bit we want it all
-        arcmap_reg = winreg.OpenKey(
-            root_key, reg_path, 0,
-            (winreg.KEY_READ | winreg.KEY_WOW64_64KEY))
-    except fnf_exception as error:
-        log_exception(error)
-        if error.errno == errno.ENOENT:
-            pass
-        else:
-            raise
-
-    if arcmap_reg:
-        installed = True
-
-    return installed
-
-
-def arcmap_path(version=None):
-    """Path to ArcGIS Installation."""
-
-    arcmap_path = None
-
-    if not version:
-        version = "10.3"
-    package_key = "Desktop{}".format(version)
-
+    Returns: True or False"""
     root_key = winreg.HKEY_LOCAL_MACHINE
+    if not version:
+        version = "10.3"
+    package_key = "Desktop{}".format(version)
+
     arc_reg_paths = [
         "SOFTWARE\\ESRI\\{}".format(package_key),
         "SOFTWARE\\Wow6432Node\\ESRI\\{}".format(package_key)
     ]
 
-    arcmap_reg = None
+    installed = False
     for reg_path in arc_reg_paths:
+        arcmap_reg = None
         try:
             # find the key, 64- or 32-bit we want it all
             arcmap_reg = winreg.OpenKey(
@@ -444,18 +416,60 @@ def arcmap_path(version=None):
                 raise
 
         if arcmap_reg:
+            installed = True
+
+    return installed
+
+
+def arcmap_path(version=None):
+    """Path to ArcGIS Installation. By default, attempts to look up all
+       possible versions that can be connected to R, or can optionally
+       check only a specific version passed with the **version** keyword.
+
+       Returns: path of installed ArcGIS Desktop installation."""
+
+    arcmap_path = None
+
+    # list supported major versions
+    releases = ["10.3", "10.4", "10.5"]
+    if releases and version in releases:
+        releases = [version]
+
+    root_key = winreg.HKEY_LOCAL_MACHINE
+    for ver in releases:
+        package_key = "Desktop{}".format(ver)
+        arc_reg_paths = [
+            "SOFTWARE\\ESRI\\{}".format(package_key),
+            "SOFTWARE\\Wow6432Node\\ESRI\\{}".format(package_key)
+        ]
+
+        arcmap_reg = None
+        for reg_path in arc_reg_paths:
             try:
-                # returns a tuple of (value, type)
-                arcmap_path_key = winreg.QueryValueEx(arcmap_reg, "InstallDir")
-                arcmap_path_raw = arcmap_path_key[0]
-                if os.path.exists(arcmap_path_raw):
-                    arcmap_path = arcmap_path_raw.strip('\\')
+                # find the key, 64- or 32-bit we want it all
+                arcmap_reg = winreg.OpenKey(
+                    root_key, reg_path, 0,
+                    (winreg.KEY_READ | winreg.KEY_WOW64_64KEY))
             except fnf_exception as error:
                 log_exception(error)
                 if error.errno == errno.ENOENT:
                     pass
                 else:
                     raise
+
+            if arcmap_reg:
+                try:
+                    # returns a tuple of (value, type)
+                    arcmap_path_key = winreg.QueryValueEx(arcmap_reg, "InstallDir")
+                    arcmap_path_raw = arcmap_path_key[0]
+                    if os.path.exists(arcmap_path_raw):
+                        arcmap_path = arcmap_path_raw.strip('\\')
+                except fnf_exception as error:
+                    log_exception(error)
+                    if error.errno == errno.ENOENT:
+                        pass
+                    else:
+                        raise
 
     return arcmap_path
 
